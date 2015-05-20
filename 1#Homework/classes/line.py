f2f04721bcb4eadcdd76406318646a174c114f80
Alessandro Menduni <menduni6@gmail.com>
2015-05-20 23:39:53 +0200

    Fix flaws in alghoritm and clean code
    The algorithm works fine and takes file name from command line, moreover a self-adjusted threshold is used. Code has been commented

--- 1#Homework/classes/line.py
+++ 1#Homework/classes/line.py
@@ -1,21 +1,23 @@
 class Line(object):
+	def __init__(self, p1, p2):
+		self.m = (p2.y - p1.y) / (p2.x - p1.x)
+		self.q = p2.y - (self.m * p2.x)
+		self.points = list()
+		
+	def contains(self, p):
+		y_ = (self.m * p.x) + self.q
+		if abs(y_ - p.y) <= float(self.t):
+			return True
+		return False
+		
+	def addPoint(self, p):
+		points.append(p)
+		
+	def pointsNumber(self):
+		return len(points)
+		
+	def __str__(self):
+		return "y = " + str(self.m) + "x + "+ str(self.q) 
 	
-		def __init__(self, p1, p2):
-			self.m = (p2.y - p1.y) / (p2.x - p1.x)
-			self.q = p2.y - (self.m * p2.x)
-			self.points = list()
-			
-		def contains(self, p):
-			if (self.m * p.x) + self.q == p.y:
-				return True
-			return False
-			
-		def addPoint(self, p):
-			points.append(p)
-			
-		def pointsNumber(self):
-			return len(points)
-			
-		def __str__(self):
-			return "y = " + str(self.m) + "x + "+ str(self.q) 
-			
+	def setThreshold(self, new_t):
+		self.t = new_t
--- 1#Homework/script.py
+++ 1#Homework/script.py
@@ -2,6 +2,7 @@
 from classes.line import Line
 from classes.point import Point
 import subprocess
+import sys
 
 #function to convert hh:mm:ss.ns to ns
 def time_conversion( str ):
@@ -10,8 +11,16 @@ def time_conversion( str ):
 	t = int(array[0])*3600+int(array[1])*60+float(array[2])
 	return t
 
+#check command line arg
+if len(sys.argv) != 2:
+	print "Usage: python script.py <pcapfile>"
+	exit()
+
+#retrive filename from command line
+filein = sys.argv[1]
+
 #call command and retrieve output
-cmd = "tshark -r capture.pcap -2 -R tcp -T fields -e ip.src -e tcp.options.timestamp.tsval -e frame.time | sort -nk2 | uniq -u | awk '{print $1, $2, $NF}' "
+cmd = "tshark -r "+filein+" -2 -R tcp -T fields -e ip.src -e tcp.options.timestamp.tsval -e frame.time | sort -nk2 | uniq -u | awk '{print $1, $2, $NF}' "
 ps = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
 output = ps.communicate()[0]
 
@@ -32,43 +41,23 @@ for s in output.split():
 	else:
 		count+=1
 
-found = list()
-slopes = list()
-ips = list()
-#find slopes of lines between consecutive timestamps
+#consider each packet as a "point" P = (time_of_capture, timestamp)
+#connect with a line packets having same source IP address 
+#if a packet doesn't belong to the line, then more than one host are behind the same IP address
+curr_ip = "init"
 for i in range(len(data) - 1):
-	if data[i]["ip"] in ips:
-		print ips
-		continue
-	if data[i]["ip"] != data[i+1]["ip"]:
-		continue
-	p1 = Point(data[i]["t"], data[i]["ts"])
-	p2 = Point(data[i+1]["t"], data[i+1]["ts"])
-	l = Line(p1, p2)
-	#print "new line:", l, p1, p2
-	for j in range (i+2, len(data)):
-		p3 = Point(data[j]["t"], data[j]["ts"])
-		#print "verifying", p3
-		if not l.contains(p3):
-			print "found natted ip:", data[j]["ip"]
-			ips.append(data[j]["ip"])
-	
-'''
-	if data[i]["ip"] not in found:
-		p1 = data[i]
-		p2 = data[i+1]
-		if p1["ip"] != p2["ip"] or p1["ts"] == p2["ts"]:
-			continue	
-		if p1["ip"] not in ips:
-			print "adding", p1["ip"]
-			ips.append(p1["ip"])
-			slopes[:] = []
-		A = (p2["ts"] - p1["ts"]) / (p2["t"] - p1["t"])
-		for s in slopes:
-			if A - s > 100 or A-s < -100:
-				found.append(p1["ip"])
-				print p1["ip"], A
-		slopes.append(A)
-'''
+	if (curr_ip != data[i]["ip"]):
+		curr_ip = data[i]["ip"]
+		if data[i]["ip"] != data[i+1]["ip"]:
+			continue
+		p1 = Point(data[i]["t"], data[i]["ts"])
+		p2 = Point(data[i+1]["t"], data[i+1]["ts"])
+		l = Line(p1, p2)
+		for j in range (i+2, len(data)):
+			p3 = Point(data[j]["t"], data[j]["ts"])
+			l.setThreshold(data[j]["ts"] * 0.3)
+			if not l.contains(p3):
+				print data[j]["ip"]
+				break
 
 
